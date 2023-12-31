8bb5dc659b41c824372f20e7799fb0e73743fc6b
Vicktor007 <vicktord007@gmail.com>
2023-12-31 14:18:59 +0100

    save

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,23 +1,3 @@
-// import jwt from "jsonwebtoken";
-
-// const generateToken = (res, userId) => {
-//   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-//     expiresIn: "30d",
-//   });
-
-//   // Set JWT as an HTTP-Only Cookie
-//   res.cookie("jwt", token, {
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV !== "development",
-//     sameSite: "strict",
-//     maxAge: 30 * 24 * 60 * 60 * 1000,
-//   });
-
-//   return token;
-// };
-
-// export default generateToken;
-
 import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
@@ -25,19 +5,39 @@ const generateToken = (res, userId) => {
     expiresIn: "30d",
   });
 
-  console.log("Generated token: ", token);
-
   // Set JWT as an HTTP-Only Cookie
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "none",
+    sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
-
-  console.log("Set cookie: ", res.cookies);
 
   return token;
 };
 
 export default generateToken;
+
+// import jwt from "jsonwebtoken";
+
+// const generateToken = (res, userId) => {
+//   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+//     expiresIn: "30d",
+//   });
+
+//   console.log("Generated token: ", token);
+
+//   // Set JWT as an HTTP-Only Cookie
+//   res.cookie("jwt", token, {
+//     httpOnly: true,
+//     secure: process.env.NODE_ENV !== "development",
+//     sameSite: "none",
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//   });
+
+//   console.log("Set cookie: ", res.cookies);
+
+//   return token;
+// };
+
+// export default generateToken;
