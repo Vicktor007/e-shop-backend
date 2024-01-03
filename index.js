05a352e39f5ec888120570d673a87decc4546c74
Vicktor007 <vicktord007@gmail.com>
2024-01-03 18:09:39 +0100

    save

--- index.js
+++ index.js
@@ -23,24 +23,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(cors({
   origin: ["http://localhost:5173", "https://e-shop-frontend-beta.vercel.app"],
   credentials: true
 }));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');
-  res.setHeader('Cross-origin-Opener-Policy','same-origin');
-
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200)
-  } else {
-    next()
-  }
-});
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
