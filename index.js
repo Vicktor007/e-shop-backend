e5c90e3e07396c7616c4f1b22e6b5fdbe2ab9b0d
Vicktor007 <vicktord007@gmail.com>
2024-01-03 18:10:27 +0100

    save

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 // Utiles
 import connectDB from "./config/db.js";
