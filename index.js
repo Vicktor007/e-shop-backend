ec9f8d93833ca8bd46e0d88a873fec233da6efdb
Vicktor007 <vicktord007@gmail.com>
2024-01-04 14:31:19 +0100

    save

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors({
