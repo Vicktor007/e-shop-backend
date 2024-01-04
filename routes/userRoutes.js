f5aa298b5bee73de45ee94655aee96e419d85c64
Vicktor007 <vicktord007@gmail.com>
2024-01-04 14:20:50 +0100

    save

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,9 +18,10 @@ import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-
-  router.post("/create",createUser)
-  router.get("/get", authenticate, authorizeAdmin, getAllUsers);
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
 
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
