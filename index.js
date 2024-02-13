a5fcaca0d9421a1c5a686cbd3281c7c38a9cade9
Vicktor007 <vicktord007@gmail.com>
2024-02-13 14:10:46 +0100

    save

--- index.js
+++ index.js
@@ -31,11 +31,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+
 
 
 app.use(cors({
