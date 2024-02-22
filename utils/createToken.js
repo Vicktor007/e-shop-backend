89cea0eeab5c8e1526b2fefca780cdbd6d17370b
Vicktor007 <vicktord007@gmail.com>
2024-02-22 13:21:42 +0100

    testing

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,57 +1,71 @@
+
+
+
 import jwt from "jsonwebtoken";
 
-const generateToken = (res, userId) => {
+const createToken = (userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "3d",
-  });
-
-  // Set JWT as an HTTP-Only Cookie
-  res.cookie("jwt", token, {
-    path: "/",
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    maxAge: 3 * 24 * 60 * 60 * 1000,
+    expiresIn: "30d",
   });
 
   return token;
 };
 
+export default createToken;
 
-
-
-const createToken = (res, userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "3d",
-  });
-  return token;
-};
-
-export {
-  generateToken,
-  createToken
-};
-
-// import jwt from "jsonwebtoken";
 
 // const generateToken = (res, userId) => {
 //   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-//     expiresIn: "30d",
+//     expiresIn: "3d",
 //   });
-
-//   console.log("Generated token: ", token);
 
 //   // Set JWT as an HTTP-Only Cookie
 //   res.cookie("jwt", token, {
+//     path: "/",
 //     httpOnly: true,
-//     secure: process.env.NODE_ENV !== "development",
+//     secure: true,
 //     sameSite: "none",
-//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     maxAge: 3 * 24 * 60 * 60 * 1000,
 //   });
-
-//   console.log("Set cookie: ", res.cookies);
 
 //   return token;
 // };
 
-// export default generateToken;
+
+
+
+// const createToken = (res, userId) => {
+//   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+//     expiresIn: "3d",
+//   });
+//   return token;
+// };
+
+// export {
+//   generateToken,
+//   createToken
+// };
+
+// // import jwt from "jsonwebtoken";
+
+// // const generateToken = (res, userId) => {
+// //   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+// //     expiresIn: "30d",
+// //   });
+
+// //   console.log("Generated token: ", token);
+
+// //   // Set JWT as an HTTP-Only Cookie
+// //   res.cookie("jwt", token, {
+// //     httpOnly: true,
+// //     secure: process.env.NODE_ENV !== "development",
+// //     sameSite: "none",
+// //     maxAge: 30 * 24 * 60 * 60 * 1000,
+// //   });
+
+// //   console.log("Set cookie: ", res.cookies);
+
+// //   return token;
+// // };
+
+// // export default generateToken;
