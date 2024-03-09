03cce24d5af6967d2d365d6e72679cb2b1bcd4ce
Vicktor007 <vicktord007@gmail.com>
2024-03-09 11:43:00 +0100

    save

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,28 +2,57 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 import asyncHandler from "./asyncHandler.js";
 
-const authenticate = asyncHandler(async (req, res, next) => {
-  let token;
+// const authenticate = asyncHandler(async (req, res, next) => {
+//   let token;
 
-  // Read JWT from the 'jwt' cookie
-  token = req.cookies.jwt;
+//   // Read JWT from the 'jwt' cookie
+//   token = req.cookies.jwt;
 
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.userId).select("-password");
-      next();
-    } catch (error) {
+//   if (token) {
+//     try {
+//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//       req.user = await User.findById(decoded.userId).select("-password");
+//       next();
+//     } catch (error) {
       
-      res.status(401);
-      throw new Error("Not authorized, token failed.");
-    }
-  } else {
-    res.status(401);
-    throw new Error("Not authorized, no token.");
-  }
-});
+//       res.status(401);
+//       throw new Error("Not authorized, token failed.");
+//     }
+//   } else {
+//     res.status(401);
+//     throw new Error("Not authorized, no token.");
+//   }
+// });
 
+
+const authenticate = asyncHandler (async(req, res, next) => {
+    try {
+        // check if login token exist
+        const token = req.cookies.jwt
+        if(!token) {
+            res.status(401) 
+            throw new Error("Not authorized, please login")      
+           }
+
+        //    verify token
+        const verifiedToken = jwt.verify(token, process.env.JWT_SECRET);
+        // get user id from token
+        const user = await User.findById(verifiedToken.userId).select("-password");
+        // check if user exist
+        if(!user) {
+            res.status(401)
+        throw new Error("User not found");
+    
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        res.status(401) 
+        throw new Error("Not authorized, please login")  
+    }
+})
+
+module.exports = protect; 
 
 
 const authorizeAdmin = (req, res, next) => {
